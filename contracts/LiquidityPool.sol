2e3b7d6bb5255459dd2ead4d4c9f7c1342a57d45
Rio <onwukachibike@gmail.com>
2023-03-23 16:19:11 +0100

    updated

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -8,7 +8,7 @@ import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
 import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
-import "hardhat/console.sol"
+import "hardhat/console.sol";
 contract LiquidityPool is IERC721Receiver {
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
