8c64b7dd9902803c4215e28e5ac9919f8b0fb6f9
Rio <onwukachibike@gmail.com>
2023-03-23 16:05:20 +0100

    updated compiler settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
 
+const DEFAULT_COMPILER_SETTINGS = {
+    version: '0.7.6',
+    settings: {
+      evmVersion: 'istanbul',
+      optimizer: {
+        enabled: true,
+        runs: 1_000_000,
+      },
+      metadata: {
+        bytecodeHash: 'none',
+      },
+    },
+}
+
 module.exports = {
   solidity: "0.7.6",
   compilers: [DEFAULT_COMPILER_SETTINGS],
