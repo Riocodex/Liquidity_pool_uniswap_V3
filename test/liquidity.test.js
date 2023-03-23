4d44187844a612b0d419d529a00a226cb0d6538e
Rio <onwukachibike@gmail.com>
2023-03-23 17:33:33 +0100

    updated

--- test/liquidity.test.js
+++ test/liquidity.test.js
@@ -66,7 +66,7 @@ describe("LiquidityPool", () => {
       await dai.connect(accounts[0]).transfer(liquidityPool.address, daiAmount)
       await usdc.connect(accounts[0]).transfer(liquidityPool.address, usdcAmount)
 
-      await liquidityExamples.mintNewPosition()
+      await liquidityPool.mintNewPosition()
 
       console.log("Dai balance after adding liquidity",await dai.balanceOf(accounts[0].address))
       console.log("USDC balance after adding liquidity",await usdc.balanceOf(accounts[0].address))
