e595aedc71dddcc7e4fe4b873d9c8c690d2625b9
Rio <onwukachibike@gmail.com>
2023-03-23 16:43:38 +0100

    updated

--- test/liquidity.test.js
+++ test/liquidity.test.js
@@ -5,7 +5,7 @@ const { ethers } = require("hardhat")
 const DAI ="0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const USDC ="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
-//contract addresses to get token
+//accounts with tokens
 const DAI_WHALE = "0x2FAF487A4414Fe77e2327F0bf4AE2a264a776AD2"
 const USDC_WHALE = "0x2FAF487A4414Fe77e2327F0bf4AE2a264a776AD2"
 
@@ -51,6 +51,9 @@ describe("LiquidityPool", () => {
       //should check if the amount we want to swap is in the contract address we want to get it from
       expect(await dai.balanceOf(daiWhale.address)).to.gte.(daiAmount)
       expect(await usdc.balanceOf(usdcWhale.address)).to.gte(usdcAmount)
+
+      await dai.connect(daiWhale).transfer(accounts[0].address, daiAmount)
+      await usdc.connect(usdcWhale).transfer(accoutns[0].address, usdcAmount)
   })
 
 
