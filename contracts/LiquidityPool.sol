429ae7608622728e28792b601a936e129bc14532
Rio <onwukachibike@gmail.com>
2023-03-25 21:30:07 +0100

    updated

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -10,7 +10,7 @@ import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.s
 import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
 import "hardhat/console.sol";
 
-contract LiquidityExamples is IERC721Receiver {
+contract LiquidityPool is IERC721Receiver {
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
