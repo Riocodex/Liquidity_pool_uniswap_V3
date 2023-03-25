8525d34bc34b9857036fa647cb6791de2aac38f4
Rio <onwukachibike@gmail.com>
2023-03-25 21:21:11 +0100

    updated

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -2,20 +2,23 @@
 pragma solidity =0.7.6;
 pragma abicoder v2;
 
-import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
-import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
-import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
-import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
 import "hardhat/console.sol";
-contract LiquidityPool is IERC721Receiver {
+
+contract LiquidityExamples is IERC721Receiver {
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
+    // 0.01% fee
     uint24 public constant poolFee = 100;
 
-    INonfungiblePositionManager public constant nonfungiblePositionManager = INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88	);
+    INonfungiblePositionManager public nonfungiblePositionManager = 
+        INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
 
     /// @notice Represents the deposit of an NFT
     struct Deposit {
@@ -26,66 +29,86 @@ contract LiquidityPool is IERC721Receiver {
     }
 
     /// @dev deposits[tokenId] => Deposit
-    mapping(uint256 => Deposit) public deposits;
+    mapping(uint => Deposit) public deposits;
 
+    // Store token id used in this example
     uint public tokenId;
 
     // Implementing `onERC721Received` so this contract can receive custody of erc721 tokens
     function onERC721Received(
         address operator,
         address,
-        uint256 tokenId,
+        uint _tokenId,
         bytes calldata
     ) external override returns (bytes4) {
-        // get position information
-
-        _createDeposit(operator, tokenId);
-
+        _createDeposit(operator, _tokenId);
         return this.onERC721Received.selector;
     }
 
-    function _createDeposit(address owner, uint256 _tokenId) internal {
-        (, , address token0, address token1, , , , uint128 liquidity, , , , ) =
-            nonfungiblePositionManager.positions(_tokenId);
+    function _createDeposit(address owner, uint _tokenId) internal {
+        (
+            ,
+            ,
+            address token0,
+            address token1,
+            ,
+            ,
+            ,
+            uint128 liquidity,
+            ,
+            ,
+            ,
 
+        ) = nonfungiblePositionManager.positions(_tokenId);
         // set the owner and data for position
         // operator is msg.sender
-        deposits[_tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
+        deposits[_tokenId] = Deposit({
+            owner: owner,
+            liquidity: liquidity,
+            token0: token0,
+            token1: token1
+        });
+
+        console.log("Token id", _tokenId);
+        console.log("Liquidity", liquidity);
+
         tokenId = _tokenId;
-
-        console.log("token id: ", _tokenId);
-        console.log("liquidity ", liquidity);
-
     }
 
-        /// @notice Calls the mint function defined in periphery, mints the same amount of each token. For this example we are providing 1000 DAI and 1000 USDC in liquidity
-    /// @return tokenId The id of the newly minted ERC721
-    /// @return liquidity The amount of liquidity for the position
-    /// @return amount0 The amount of token0
-    /// @return amount1 The amount of token1
     function mintNewPosition()
         external
         returns (
-            uint256 tokenId,
+            uint _tokenId,
             uint128 liquidity,
-            uint256 amount0,
-            uint256 amount1
+            uint amount0,
+            uint amount1
         )
     {
         // For this example, we will provide equal amounts of liquidity in both assets.
         // Providing liquidity in both assets means liquidity will be earning fees and is considered in-range.
-        uint256 amount0ToMint = 100 * 1e18;
-        uint256 amount1ToMint = 100 ^1e6;
+        uint amount0ToMint = 100 * 1e18;
+        uint amount1ToMint = 100 * 1e6;
 
         // Approve the position manager
-        TransferHelper.safeApprove(DAI, address(nonfungiblePositionManager), amount0ToMint);
-        TransferHelper.safeApprove(USDC, address(nonfungiblePositionManager), amount1ToMint);
+        TransferHelper.safeApprove(
+            DAI,
+            address(nonfungiblePositionManager),
+            amount0ToMint
+        );
+        TransferHelper.safeApprove(
+            USDC,
+            address(nonfungiblePositionManager),
+            amount1ToMint
+        );
 
-        INonfungiblePositionManager.MintParams memory params =
-            INonfungiblePositionManager.MintParams({
+        INonfungiblePositionManager.MintParams
+            memory params = INonfungiblePositionManager.MintParams({
                 token0: DAI,
                 token1: USDC,
                 fee: poolFee,
+                // By using TickMath.MIN_TICK and TickMath.MAX_TICK, 
+                // we are providing liquidity across the whole range of the pool. 
+                // Not recommended in production.
                 tickLower: TickMath.MIN_TICK,
                 tickUpper: TickMath.MAX_TICK,
                 amount0Desired: amount0ToMint,
@@ -96,34 +119,39 @@ contract LiquidityPool is IERC721Receiver {
                 deadline: block.timestamp
             });
 
-        // Note that the pool defined by DAI/USDC and fee tier 0.3% must already be created and initialized in order to mint
-        (tokenId, liquidity, amount0, amount1) = nonfungiblePositionManager.mint(params);
+        // Note that the pool defined by DAI/USDC and fee tier 0.01% must 
+        // already be created and initialized in order to mint
+        (_tokenId, liquidity, amount0, amount1) = nonfungiblePositionManager
+            .mint(params);
 
         // Create a deposit
-        _createDeposit(msg.sender, tokenId);
+        _createDeposit(msg.sender, _tokenId);
 
         // Remove allowance and refund in both assets.
         if (amount0 < amount0ToMint) {
-            TransferHelper.safeApprove(DAI, address(nonfungiblePositionManager), 0);
-            uint256 refund0 = amount0ToMint - amount0;
+            TransferHelper.safeApprove(
+                DAI,
+                address(nonfungiblePositionManager),
+                0
+            );
+            uint refund0 = amount0ToMint - amount0;
             TransferHelper.safeTransfer(DAI, msg.sender, refund0);
         }
 
         if (amount1 < amount1ToMint) {
-            TransferHelper.safeApprove(USDC, address(nonfungiblePositionManager), 0);
-            uint256 refund1 = amount1ToMint - amount1;
+            TransferHelper.safeApprove(
+                USDC,
+                address(nonfungiblePositionManager),
+                0
+            );
+            uint refund1 = amount1ToMint - amount1;
             TransferHelper.safeTransfer(USDC, msg.sender, refund1);
         }
     }
 
-        /// @notice Collects the fees associated with provided liquidity
-    /// @dev The contract must hold the erc721 token before it can collect fees
-    /// @param tokenId The id of the erc721 token
-    /// @return amount0 The amount of fees collected in token0
-    /// @return amount1 The amount of fees collected in token1
     function collectAllFees() external returns (uint256 amount0, uint256 amount1) {
-   
-        
+        // set amount0Max and amount1Max to uint256.max to collect all fees
+        // alternatively can set recipient to msg.sender and avoid another transaction in `sendToOwner`
         INonfungiblePositionManager.CollectParams memory params =
             INonfungiblePositionManager.CollectParams({
                 tokenId: tokenId,
@@ -133,8 +161,14 @@ contract LiquidityPool is IERC721Receiver {
             });
 
         (amount0, amount1) = nonfungiblePositionManager.collect(params);
-        
+
         console.log("fee 0", amount0);
         console.log("fee 1", amount1);
     }
+
+   
+
+  
+
+   
 }
